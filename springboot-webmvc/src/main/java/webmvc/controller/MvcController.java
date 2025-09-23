9c8d5e9ac1088174431dde6cbeb54776230e6991
sung9920 <sungcheol9920@gmail.com>
2025-09-23 12:18:12 +0900

    springbott-webmvc #1

---
+++ springboot-webmvc/src/main/java/webmvc/controller/MvcController.java
@@ -0,0 +1,15 @@
+package webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MvcController {
+
+	@ResponseBody
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello, world";
+	}
+}
